0a4803a3163c66613fd0fd2a09fb729a8f4a1b94
Mohamed Assem <mo7mad.assim@gmail.com>
2018-10-20 09:13:41 +0200

    Finalize detail activity

---
+++ app/src/main/java/udacity/assem/com/udaceity_baking_app/Activities/DetailsActivity.java
@@ -0,0 +1,60 @@
+package udacity.assem.com.udaceity_baking_app.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import tests.assem.com.udaceity_baking_app.R;
+import udacity.assem.com.udaceity_baking_app.App.AppConfig;
+import udacity.assem.com.udaceity_baking_app.Fragments.IngredientsFragment;
+import udacity.assem.com.udaceity_baking_app.Models.RecipeModel;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    public static Fragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        Intent bundleIntent = getIntent();
+        RecipeModel recipeModel = (RecipeModel) bundleIntent.getSerializableExtra(AppConfig.INTENT_BUNDLE_KEY);
+        if (recipeModel != null) {
+            manageFragments(recipeModel);
+        } else {
+            closeOnError();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        overridePendingTransition(R.anim.enter_from_right, R.anim.exit_to_right);
+    }
+
+    private void closeOnError() {
+        finish();
+        Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void manageFragments(RecipeModel recipeModel) {
+        // get fragment manager
+        FragmentManager fm = getSupportFragmentManager();
+        // add
+        FragmentTransaction ft = fm.beginTransaction();
+        if (fragment == null)
+            fragment = new IngredientsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(AppConfig.INTENT_BUNDLE_KEY, recipeModel);
+        IngredientsFragment ingredientsFragment = new IngredientsFragment();
+        ingredientsFragment.setArguments(bundle);
+        fragment.setArguments(bundle);
+        ft.replace(R.id.start_content, fragment);
+        ft.setCustomAnimations(R.anim.fade_out, R.anim.fade_in);
+        ft.commit();
+    }
+}

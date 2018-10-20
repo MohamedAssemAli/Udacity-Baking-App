c3abfdf3e553cb6944c9d866c469eb60dce10167
Mohamed Assem <mo7mad.assim@gmail.com>
2018-10-20 09:10:24 +0200

    Ingredient module

---
+++ app/src/main/java/udacity/assem/com/udaceity_baking_app/Models/IngredientModel.java
@@ -0,0 +1,53 @@
+package udacity.assem.com.udaceity_baking_app.Models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class IngredientModel implements Serializable {
+
+    @SerializedName("quantity")
+    @Expose
+    private double quantity;
+    @SerializedName("measure")
+    @Expose
+    private String measure;
+    @SerializedName("ingredient")
+    @Expose
+    private String ingredient;
+
+    public IngredientModel() {
+    }
+
+    public IngredientModel(double quantity, String measure, String ingredient) {
+        super();
+        this.quantity = quantity;
+        this.measure = measure;
+        this.ingredient = ingredient;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+}
